0da5b277c9a5eeecf0a3cea97cbe0252f66dddff
Myokatoshi <sylvain.motta@gmail.com>
2021-11-30 18:46:54 +0100

    Mise en place de mongoose et de la connexion avec MangoDB

--- back-end/app.js
+++ back-end/app.js
@@ -1,24 +1,54 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Thing = require('./models/thing');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
+mongoose.connect('mongodb+srv://Myo:Myo@cluster.04hre.mongodb.net/ClusterHotTakes?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
-  res.status(201);
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
+app.post('/api/sauces', (req, res, next) => {
+    delete req.body._id;
+    const thing = new Thing({
+      ...req.body
+    });
+    thing.save()
+      .then(() => res.status(201).json({ message: 'Recette de sauce enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
 });
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
+app.get('/api/sauces', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
 });
 
 module.exports = app;
